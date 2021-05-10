64af80cb781ecf124f86af3362b4d0844f91f2ed
ПК <kravchunasg@gmail.com>
2021-05-10 14:04:44 +0300

    Init. commit

---
+++ src/test/java/com/example/junapp/JunAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
