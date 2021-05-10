2b403a8065302567b7ad850951586652825ea486
ПК <kravchunasg@gmail.com>
2021-05-10 21:59:41 +0300

    other change

--- src/main/java/com/example/junapp/JunApplication.java
+++ src/main/java/com/example/junapp/JunApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JunAppApplication {
+public class JunApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JunAppApplication.class, args);
+        SpringApplication.run(JunApplication.class, args);
     }
 
 }
