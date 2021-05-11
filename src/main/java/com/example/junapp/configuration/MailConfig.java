ff7d1fc3863fcd38f0281b2631a3c105b10db177
ПК <kravchunasg@gmail.com>
2021-05-11 20:51:05 +0300

    Add mail sender and config

---
+++ src/main/java/com/example/junapp/configuration/MailConfig.java
@@ -0,0 +1,32 @@
+package com.example.junapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("kravchunasg@gmail.com");
+        mailSender.setPassword("vefbxmsmzyvlnckw");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
